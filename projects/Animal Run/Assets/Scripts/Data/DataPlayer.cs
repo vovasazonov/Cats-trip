6db6113c93f8574fbe9b1eff182236093bd59e02
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2019-05-26 12:22:18 +0400

    Created connection with mongodb and unity

--- projects/Animal Run/Assets/Scripts/Data/DataPlayer.cs
+++ projects/Animal Run/Assets/Scripts/Data/DataPlayer.cs
@@ -6,6 +6,7 @@
 
 using System.Collections.Generic;
 using System;
+using UnityEngine;
 
 /// <summary>
 /// Data about dataplayer interaction
---
+++ projects/Animal Run/Assets/Scripts/MongoDB/ServerMongoDB.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System; //
+using System.Collections;
+using System.Collections.Generic;  // Lists
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+
+public class ServerMongoDB : MonoBehaviour, IDatabaseable
+{
+	private string _connectionString = "mongodb://localhost:27017";
+	private MongoDatabase _database = null;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		// Start server
+		_database = new MongoClient(_connectionString).GetServer().GetDatabase("catstripdb");
+		Debug.Log("ESTABLISHED CONNECTION TO MONGODB");
+	}
+
+
+
+}
